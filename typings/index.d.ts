d7108fcdb50d3cf091f8a3985ac78bf7fcabc55d
thomas <sketchbang@gmail.com>
2016-08-12 12:40:10 -0500

    ???

--- typings/index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="globals/core-js/index.d.ts" />
-/// <reference path="globals/jasmine/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/source-map/index.d.ts" />
-/// <reference path="globals/uglify-js/index.d.ts" />
-/// <reference path="globals/webpack/index.d.ts" />
